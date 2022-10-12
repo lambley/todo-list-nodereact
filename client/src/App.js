584df211a955813a3d15b76fe87522aed7921195
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-12 12:12:44 +0100

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-function App() {
-  return (
-      <div>
-          <p>Hello World!</p>
-      </div>
-  );
-}
+import React from "react";
+import socketIO from "socket.io-client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./components/Main";
+import Home from "./components/Home";
+
+const socket = socketIO.connect("http://localhost:4001");
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/app' element={<Main socket={socket} />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
 export default App;
