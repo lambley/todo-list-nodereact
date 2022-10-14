baf95f3bbf05b1c7a0d9fb8a15ac197326dcd451
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-14 09:05:34 +0100

    add basic modal and css

---
+++ client/src/components/Modal.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const Modal = ({ socket }) => {
+  const [comment, setComment] = useState("");
+
+  const addComment = (e) => {
+    e.preventDefault();
+    console.log({ comment });
+    setComment("");
+  };
+
+  return (
+    <div className='modal'>
+      <div className='modal__container'>
+        <h3>Comments</h3>
+        <form className='comment__form' onSubmit={addComment}>
+          <input
+            className='comment__input'
+            type='text'
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            required
+          />
+          <button>Add Comment</button>
+        </form>
+        <div className='comments__container'>
+          <div className='comment'>
+            <p>
+                <strong>Nevo - </strong> Hello guys
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
